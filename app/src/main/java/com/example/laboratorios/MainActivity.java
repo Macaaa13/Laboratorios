fbb329b5daa11127ac169386ae78cc3794e594e6
Macaaa13 <maca.moya@hotmail.com>
2022-11-04 15:43:01 -0300

    +toolbar

--- app/src/main/java/com/example/laboratorios/MainActivity.java
+++ app/src/main/java/com/example/laboratorios/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.laboratorios;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.example.laboratorios.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar myToolbar;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        myToolbar = binding.toolbar;
+        setSupportActionBar(myToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
